a043493a7d7d1909ae6c3a9c9129a90336654cb6
Zwelakhe Msuthu <Zwenix@gmail.com>
2026-03-26 02:47:39 +0000

    build(config):add standalone output and adjust Firebase Hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    swcTracingFlush: false,
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.pexels.com' },
